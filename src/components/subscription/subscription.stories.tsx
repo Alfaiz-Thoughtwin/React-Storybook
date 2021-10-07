e1bfc0d76c75b632b6d6632a757f9df3c5214ed6
Alfaiz.Thoughtwin <alfaiz.thoughtwin@gmail.com>
2021-10-07 14:00:22 +0530

    Story Within Story -> Button + Input = Subscription-Form Story

---
+++ src/components/subscription/subscription.stories.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Large } from "../input/Input.stories";
+import { Danger } from "../button/Button.stories";
+
+
+export default {
+    title : "React-Storybook/UI/Forms/Subscription-Form",
+};
+
+
+export const DangerSubscription = () => (
+    <>
+        <Large />
+        <Danger />
+    </>
+);
