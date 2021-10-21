2256c6f1ac1854592e4393c09aeec70ad00be401
Alfaiz.Thoughtwin <alfaiz.thoughtwin@gmail.com>
2021-10-21 13:55:08 +0530

    .storybook/preview.js --> default decorators added (means every story wrapped with decorator)

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -1,22 +1,29 @@
 import React from "react";
+import { Meta, Story } from "@storybook/react";
 import { Button } from "./Button";
+// import { Center } from '../center';
+
 
 export default {
   title: "React-Storybook/UI/Controls/Buttons/Button",
   component: Button,
   args: {
       children: "Button",
+      variant:'string'
   },
-};
+  // decorators: [story=><Center>{ story() }</Center>],
+} as Meta;
 
 // Approach 1 :
 export const Primary = () => <Button variant="primary">Primary</Button>;
+// export const CenterPrimary = () => <Center><Button variant="primary">Primary</Button></Center>;
 export const Secondary = () => <Button variant="secondary">Secondary</Button>;
 export const Success = () => <Button variant="success">Success</Button>;
 export const Danger = () => <Button variant="danger">Danger</Button>;
+export const Dark = () => <Button variant="dark">Dark</Button>
 
 // Approach 2 :
-const Template = (args: any): any => <Button {...args} />;
+const Template: Story = (args:any):any => <Button {...args} />;
 
 export const PrimaryA = Template.bind({});
 PrimaryA.args = {
@@ -34,4 +41,16 @@ export const LongPrimaryA = Template.bind({});
 LongPrimaryA.args = {
   ...PrimaryA.args,
   children: "Long Primary Args",
+};
+
+export const DarkA = Template.bind({});
+DarkA.args = {
+  variant: "dark",
+  children: "Dark Args",
+};
+
+export const LongDarkA = Template.bind({});
+LongDarkA.args = {
+  ...DarkA.args,
+  children: "Long Dark Args",
 };
